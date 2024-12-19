bdc91a4f511fe0ee2ca27d9060f97e46b3afce41
Eloiza <eloiza.souza@zup.com.br>
2024-12-19 13:22:43 -0300

    test: create tests on Main to Book, User and Library

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,34 @@
 public class Main {
     public static void main(String[] args) {
         Book livro1 = new Book("Primeiro livro", "Fulano de Tal", "00001");
-//        livro1.showDetails();
-//        livro1.showStatus();
-//        livro1.lend();
-//        livro1.showStatus();
-//        livro1.setAuthor("Fulano de Tal Alterado");
-//        livro1.setTitle("Primeiro livro alterado");
-//        livro1.returnBook();
-//        livro1.showDetails();
-//        livro1.showStatus();
 
+        Book livro2 = new Book("Primeiro livro modificado", "Fulano de Tal", "00001");
+        Book livro3 = new Book("Outro livro", "Ciclano", "00002");
+        Book livro4 = new Book("Outro livro novo", "Desconhecido", "00003");
+
+        Book livro5 = new Book("Livro novo", "Autor nome", "00004");
         User user1 = new User("Jose", 1);
-        user1.addBook(livro1);
-        user1.showDetails();
 
+        Library library = new Library();
+        library.registerNewBook(livro1);
+        library.registerNewBook(livro2);
+        library.registerNewBook(livro3);
+        library.registerNewBook(livro4);
+        library.registerNewBook(livro5);
+        library.registerNewUser(user1);
+        library.returnBook("00001", 1);
+        library.lendBook("00005", 1);
+        library.lendBook("00001", 2);
+        library.lendBook("00001", 1);
+        library.lendBook("00002", 1);
+        library.lendBook("00003", 1);
+        library.lendBook("00001", 1);
+        library.returnBook("00002", 1);
+        library.lendBook("00001", 1);
+        library.lendBook("00002", 1);
+        library.showAvailableBooks();
+
+        library.returnBook("00001", 1);
+        library.showAvailableBooks();
     }
 }
