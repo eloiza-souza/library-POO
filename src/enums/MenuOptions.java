cb5eaaf33a3bbb937ef8f595d92711e7b5bb4b37
Eloiza <eloiza.souza@zup.com.br>
2024-12-19 15:24:57 -0300

    feat: add new option to MenuOptions

--- src/enums/MenuOptions.java
+++ src/enums/MenuOptions.java
@@ -3,18 +3,19 @@ package enums;
 public enum MenuOptions {
     BOOK_REGISTER("Cadastrar livro"),
     USER_REGISTER("Cadastrar usuário"),
-    LEAD_BOOK ("Realizar empréstimo"),
-    RETURN_BOOK ("Realizar devolução"),
+    LEAD_BOOK("Realizar empréstimo"),
+    RETURN_BOOK("Realizar devolução"),
     LIBRARY_BOOKS("Listar livros da biblioteca"),
     AVAILABLE_BOOKS("Listar livros disponíveis"),
-    BOOKS_USER ("Listar livros por usuário");
-
+    BOOKS_USER("Listar livros por usuário"),
+    LIBRARY_USERS("Listar usuários do sistema");
 
     private final String description;
 
     MenuOptions(String description) {
         this.description = description;
     }
+
     public String getDescription() {
         return description;
     }
