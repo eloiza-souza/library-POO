8c3fac6ea2f16f16dee920ff7d72ce0d7a915b68
Eloiza <eloiza.souza@zup.com.br>
2024-12-20 17:47:17 -0300

    fix: fixes the insertion order in the string

--- src/util/BookFileUtil.java
+++ src/util/BookFileUtil.java
@@ -8,33 +8,34 @@ import java.util.List;
 import static util.FileUtil.SEPARATOR;
 
 public class BookFileUtil {
-    final static  String FILE_NAME = "books";
+    final static String FILE_NAME = "books";
+
     public static void saveBooksToFile(List<Book> booksList) {
-        FileUtil.saveStringObjectsToFile(convertListBooksToListStrings(booksList),FILE_NAME);
+        FileUtil.saveStringObjectsToFile(convertListBooksToListStrings(booksList), FILE_NAME);
     }
 
     public static List<Book> loadBooksFromFile(String fileName) {
-        return convertListStringsToListBooks( FileUtil.loadStringObjectsFromFile(FILE_NAME));
+        return convertListStringsToListBooks(FileUtil.loadStringObjectsFromFile(FILE_NAME));
     }
 
-    private static List<String> convertListBooksToListStrings(List<Book> booksList){
+    private static List<String> convertListBooksToListStrings(List<Book> booksList) {
         List<String> listString = new ArrayList<>();
-        for (Book book: booksList){
-            listString.add(book.getIsbn()+ SEPARATOR +
-                           book.getTitle() + SEPARATOR +
+        for (Book book : booksList) {
+            listString.add(book.getTitle() + SEPARATOR +
                            book.getAuthor() + SEPARATOR +
+                           book.getIsbn() + SEPARATOR +
                            book.isAvailable());
         }
         return listString;
     }
 
-    private static List<Book> convertListStringsToListBooks(List<String> stringsList){
+    private static List<Book> convertListStringsToListBooks(List<String> stringsList) {
         List<Book> listBooks = new ArrayList<>();
-        for (String string: stringsList){
+        for (String string : stringsList) {
             String[] stringSplit = string.split(SEPARATOR);
-            if (stringSplit.length == 4){
+            if (stringSplit.length == 4) {
                 Book book = new Book(stringSplit[0], stringSplit[1], stringSplit[2]);
-                if(stringSplit[3].equalsIgnoreCase("false")){
+                if (stringSplit[3].equalsIgnoreCase("false")) {
                     book.lend();
                 }
                 listBooks.add(book);
